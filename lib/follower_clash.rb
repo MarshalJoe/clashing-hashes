1ac769a2857721fb3218f5e776f6f7bad438b852
MakerSquare Student <students@themakersquare.com>
2013-09-19 10:56:10 -0500

    Adds tests to the Comparer class

--- lib/follower_clash.rb
+++ lib/follower_clash.rb
@@ -4,10 +4,18 @@ require 'twitter'
 
 module FollowerClash
 
-class User
-end
+  class User
+    attr_reader :login
+    def initialize(login)
+      @login = login
+      @followers = 0
+  end
 
-class Comparer
-end
+  class Comparer
+    def initialize(user1, user2)
+      @user1 = user1
+      @user2 = user2
+    end
+  end
 
 end
