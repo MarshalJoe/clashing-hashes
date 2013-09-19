afe29547b3ba7b72454f22a006f1116f73b9681d
MakerSquare Student <students@themakersquare.com>
2013-09-19 08:40:55 -0500

    Completes the first Sinatra lesson's level of functionality

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -4,5 +4,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  'hello world'
+  erb :index
+end
+
+post '/twitter' do
+  @hash_one = params[:first_hash]
+  @hash_two = params[:second_hash]
+  erb :twitter_results
 end
